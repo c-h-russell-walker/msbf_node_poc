f7561691b36c3503047fbbfcfae31fdfeb234d01
Chris Russell-Walker <c.russell.walker@gmail.com>
2018-04-24 10:30:14 -0400

    CRW POC for QnA connected service

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var builder_cognitiveservices = require("botbuilder-cognitiveservices");
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -17,8 +18,42 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    console.log(session.message);
-    console.log(session.message.address.conversation.id);
-    session.send("You said: %s", session.message.text);
+// var bot = new builder.UniversalBot(connector, function (session) {
+//     console.log(session.message);
+//     console.log(session.message.address.conversation.id);
+//     session.send("You said: %s", session.message.text);
+// });
+
+var bot = new builder.UniversalBot(connector);
+
+var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+    knowledgeBaseId: process.env.QnAKnowledgebaseId,
+    subscriptionKey: process.env.QnASubscriptionKey
 });
+
+var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
+    recognizers: [recognizer],
+    defaultMessage: 'Sorry, no match - try changing the query terms.',
+    qnaThreshold: 0.3
+});
+
+bot.dialog('basicQnAMakerDialog', basicQnAMakerDialog);
+
+bot.dialog('/',
+[
+    function (session) {
+        var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
+        var qnaSubscriptionKey = process.env.QnASubscriptionKey;
+
+        // QnA Subscription Key and KnowledgeBase Id null verification
+        if (
+            (qnaSubscriptionKey == null || qnaSubscriptionKey == '')
+            ||
+            (qnaKnowledgebaseId == null || qnaKnowledgebaseId == '')
+        ) {
+            session.send('Please set QnAKnowledgebaseId and QnASubscriptionKey in App Settings. Get them at https://qnamaker.ai.');
+        } else {
+            session.replaceDialog('basicQnAMakerDialog');
+        }
+    }
+]);
