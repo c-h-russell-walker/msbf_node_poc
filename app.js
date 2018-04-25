b430e9235e6d62999f4454532b9405bd3ff725de
Chris Russell-Walker <c.russell.walker@gmail.com>
2018-04-25 16:23:29 -0400

    CRW Bot storage addition added as well as some initial flow w/ prompt

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var restify = require('restify');
+var azure = require('botbuilder-azure');
 var builder = require('botbuilder');
 var builder_cognitiveservices = require("botbuilder-cognitiveservices");
+
+// Setup DB Storage
+var documentDbOptions = {
+    host: process.env.DBHost,
+    masterKey: process.env.DBKey,
+    database: 'botdocs',
+    collection: 'botdata'
+};
+var docDbClient = new azure.DocumentDbClient(documentDbOptions);
+var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -17,14 +28,11 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-// var bot = new builder.UniversalBot(connector, function (session) {
-//     console.log(session.message);
-//     console.log(session.message.address.conversation.id);
-//     session.send("You said: %s", session.message.text);
-// });
-
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(
+    connector,
+).set(
+    'storage', cosmosStorage
+);
 
 var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
     knowledgeBaseId: process.env.QnAKnowledgebaseId,
@@ -39,21 +47,74 @@ var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
 
 bot.dialog('basicQnAMakerDialog', basicQnAMakerDialog);
 
-bot.dialog('/',
-[
+bot.dialog('faqDialog', [
     function (session) {
-        var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
-        var qnaSubscriptionKey = process.env.QnASubscriptionKey;
+        builder.Prompts.text(session, 'Enter any questions you may have.')
+    },
+    function (session, results) {
+        session.conversationData.dialog = 'basicQnAMakerDialog';
+        session.replaceDialog('basicQnAMakerDialog');
+    }
+]);
 
-        // QnA Subscription Key and KnowledgeBase Id null verification
-        if (
-            (qnaSubscriptionKey == null || qnaSubscriptionKey == '')
-            ||
-            (qnaKnowledgebaseId == null || qnaKnowledgebaseId == '')
-        ) {
-            session.send('Please set QnAKnowledgebaseId and QnASubscriptionKey in App Settings. Get them at https://qnamaker.ai.');
-        } else {
-            session.replaceDialog('basicQnAMakerDialog');
+
+var buttonOptions = [
+    'Get an Estimate',
+    'Ask a Question',
+];
+bot.dialog('startBot', [
+    function (session, args, next) {
+        builder.Prompts.choice(
+            session,
+            "Welcome to Spray-Net, what can we help you with today?",
+            buttonOptions.join('|'),
+            {
+                listStyle: builder.ListStyle.button
+            }
+        );
+    },
+    function (session, results, next) {
+        switch (results.response.index) {
+            case 0: // 'Get an Estimate'
+                session.beginDialog('getAnEstimate');
+                break;
+            case 1: // 'Ask a Question'
+                session.beginDialog('faqDialog');
+                break;
+            default:
+                session.endDialog();
+                break;
         }
     }
 ]);
+
+
+// We use this hook to check for start of conversation
+bot.on('conversationUpdate', function (message) {
+    // Start of conversation
+    var membersAdded = message.membersAdded;
+
+    if (
+        membersAdded
+        &&
+        membersAdded.length > 0
+        &&
+        membersAdded[0].id === message.address.bot.id
+    ) {
+        bot.beginDialog(message.address, 'startBot');
+    }
+});
+
+
+// Base entry point for bot interaction
+bot.dialog('/', function (session) {
+    switch (session.conversationData.dialog) {
+        case 'basicQnAMakerDialog':
+            session.beginDialog(session.conversationData.dialog);
+            break;
+        default:
+            session.send("Goodbye.")
+            session.endDialog();
+            break;
+    }
+});
