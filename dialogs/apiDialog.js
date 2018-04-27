91455c5848ec2b1bfb7907272aa3af15e730604d
Chris Russell-Walker <c.russell.walker@gmail.com>
2018-04-27 11:24:02 -0400

    CRW After confirming API calls work comment out actual call

--- dialogs/apiDialog.js
+++ dialogs/apiDialog.js
@@ -11,19 +11,23 @@ exports.create = function (bot) {
         var queryParam = encodeURIComponent(session.message.text);
         // queryParam does nothing
         // var url = "https://jsonplaceholder.typicode.com/users&q=" + queryParam;
-        var url = "https://jsonplaceholder.typicode.com/users";
-        request(url, { json: true, }, (err, res, body) => {
-            if (err) {
-                return console.log(err);
-            } else {
-                var message;
-                if (res.body && res.body.length) {
-                    message = JSON.stringify(res.body[0]);
-                } else {
-                    message = 'No results found';
-                }
-                session.send(message);
-            }
-        });
+
+        // var url = "https://jsonplaceholder.typicode.com/users";
+        // request(url, { json: true, }, (err, res, body) => {
+        //     if (err) {
+        //         return console.log(err);
+        //     } else {
+        //         var message;
+        //         if (res.body && res.body.length) {
+        //             message = JSON.stringify(res.body[0]);
+        //         } else {
+        //             message = 'No results found';
+        //         }
+        //         session.send(message);
+        //     }
+        // });
+
+        var message = "Placeholder message for API call Dialog.";
+        session.send(message);
     });
 }
